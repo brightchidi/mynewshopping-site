fdf10b4fbc65ebda5f0a3b5c95bc80bb2f979195
bright <chidib983@gmail.com>
2023-12-08 03:44:10 -0800

    added components Data.js

---
+++ frontend/src/components/Data.js
@@ -0,0 +1,220 @@
+import Gokublack from './images/Gokublack.jpg'
+import test from"./images/test.jpg"
+import wallpaper15 from "./images/wallpaper15.jpg"
+import wallpaper1 from "./images/wallpaper1.jpg"
+import wallpaper43 from "./images/wallpaper43.jpg"
+import wallpaper48 from "./images/wallpaper48.jpg"
+import wallpaper49 from "./images/wallpaper49.jpg"
+import wallpaper50 from "./images/wallpaper50.jpg"
+import wallpaper52 from "./images/wallpaper52.jpg"
+import wallpaper53 from "./images/wallpaper53.jpg"
+import wallpaper57 from "./images/wallpaper57.jpg"
+import wallpaper58 from "./images/wallpaper58.jpg"
+import wallpaper54 from "./images/wallpaper54.jpg"
+import wallpaper8 from "./images/wallpaper8.jpg"
+import wallpaper37 from "./images/wallpaper37.jpg"
+import wallpaper33 from "./images/wallpaper33.jpg"
+import wallpaper40 from "./images/wallpaper40.jpg"
+import wallpaper41 from "./images/wallpaper41.jpg"
+import wallpaper42 from "./images/wallpaper42.jpg"
+import wallpaper59 from "./images/wallpaper59.jpg"
+import wallpaper61 from "./images/wallpaper61.jpg"
+const myData ={
+    myProductItems:[ 
+        {
+             id: 1,
+             name: "Bc Comics",
+             price: 555,
+             image: Gokublack,
+         
+            Description:"we got the best thing,suit for you to grab, a place of entertainment . "
+        },
+        {
+            id: 2,
+            name: "Tom And Jerry",
+            price: 655,
+            image: wallpaper15,
+            Description:"we got everthing that you need, a place of adventure and imagination ."
+       },
+       {
+        id: 3,
+        name: "Man's Best Friend",
+        price: 755,
+        image: wallpaper43,
+        Description:"    pls purchase what is good for you, we are a friend in your need."
+   },
+   {
+    id: 4,
+    name: "Simba",
+    price: 855,
+    image: wallpaper48,
+
+    Description:" this turns your imagination into an adventure, movies and games we got just for you."
+
+
+},
+{
+    id: 5,
+    name: "Sponge Bob",
+    price: 955,
+    image: wallpaper49,
+
+    Description:"  a place where all your imagination comes to life, play the games you love."
+
+
+},
+{
+    id: 6,
+    name:"Hero",
+    price: 1055,
+    image:wallpaper50,
+
+    Description:" the best thing suit for you and  just waiting for you to grab, choose your best heroic stuffs you love. "
+
+
+},
+{
+    id: 7,
+    name: "Birds",
+    price: 1155,
+    image:wallpaper52,
+
+    Description:" trying on our product, brings joy to you and us, so don't be afraid  to try ."
+
+
+},
+{
+    id: 8,
+    name: "Baby Mouse",
+    price: 1255,
+    image: wallpaper53,
+
+    Description: "we got the cuiset thing, just for you,to try on, come and select what fit you."
+
+
+},
+{
+    id: 9,
+    name: "Spidey's And Friends",
+    price: 1355,
+    image: wallpaper57 ,
+
+    Description:"we got the best entertainment, food, games and exciting shoes, clothing and furnitures."
+
+
+},
+{
+    id: 10,
+    name: "Kaioken",
+    price: 1455,
+    image: wallpaper58,
+
+    Description:"fun  never stops burning in us, please look for the nearest shop to get your items now. "
+
+
+},{
+    id: 11,
+    name: "minions",
+    price: 1555,
+    image: wallpaper59 ,
+
+    Description: "grab your friends and enter the room of fun and adventure, lot of things you want to do."
+
+
+},{
+    id: 12,
+    name: "Strawberry Cupcake",
+    price: 1655,
+    image: wallpaper1 ,
+
+    Description: "don't sit around, waiting. choose your items and grab the entertainment you want to have."
+
+
+},
+{
+    id: 13,
+    name: "Master And Student",
+    price: 1755,
+    image: wallpaper37,
+    Description: "before your everthing is sold out and kicks you on the butt,grab your's now."
+
+
+
+},{
+    id: 14,
+    name: "Dark Knight",
+    price: 1855,
+    image: wallpaper40,
+
+    Description: "so don't hide in the shadow, your series of movies, we got for you to watch."
+
+
+},{
+    id: 15,
+    name: "Goku Super Sayian Blue",
+    price: 1955,
+    image: wallpaper54,
+
+    Description: "be there and don't miss the fun, that is coming at your door, grab our logistic furniture and cloth. "
+
+
+},
+{
+    id: 16,
+    name: "Police Force",
+    price: 2055,
+    image: wallpaper42,
+    Description: "don't be freaked out in knowing what to buy, before it late to buy. "
+
+
+
+},{
+    id: 17,
+    name: "Patrick star",
+    price: 2155,
+    image: wallpaper41 ,
+
+    Description: "we will give that smile and entertainment to are just waiting for."
+
+
+},{
+    id: 18,
+    name: "The Incredibles ",
+    price: 2255,
+    image:wallpaper8,
+
+    Description: "grab your family to the place where all of their dreams come true.  "
+
+},{
+    id: 19,
+    name: "Shining Star",
+    price: 2355,
+    image:wallpaper33,
+
+    Description: "so buy and shine like the star you are, don't be surprise of the things coming to your door."
+
+
+},{
+    id: 20,
+    name: "Duck And Dog",
+    price: 2455,
+    image: wallpaper61,
+
+    Description: "eat the best food ever been made by us , play the games you want and more"
+
+
+},
+{
+    id: 21,
+    name: "ultra instinct",
+    price: 2455,
+    image: test,
+
+    Description: "the magic never stops flowin at bc com shoprite, so come and join us in our celebration today."
+
+
+},
+
+    ]
+}
+export default myData;
